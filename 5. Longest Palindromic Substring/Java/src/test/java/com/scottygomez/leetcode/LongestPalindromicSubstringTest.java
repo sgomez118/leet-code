6ab4b3097852ed1d21beea3a27b313a634dbcb24
Scotty Gomez <scottygomez007@gmail.com>
2020-12-15 23:20:55 -0800

    Add unit test for is palindrome

--- 5. Longest Palindromic Substring/Java/src/test/java/com/scottygomez/leetcode/LongestPalindromicSubstringTest.java
+++ 5. Longest Palindromic Substring/Java/src/test/java/com/scottygomez/leetcode/LongestPalindromicSubstringTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LongestPalindromicSubstringTest {
     
@@ -21,17 +22,29 @@ public class LongestPalindromicSubstringTest {
         assertTrue(isPalindrome);
     }
 
-    @Test
-    public void isPalindrome_WhenStringIsPalindrome_ShouldReturnTrue() {
+    @ParameterizedTest
+    @ValueSource(strings = {"civic", "kayak", "level", "madam", "mom", "noon", "racecar", "radar", "redder", "refer", "repaper", "rotator", "rotor", "sagas", "solos", "stats","tenet", "wow"})
+    public void isPalindrome_WhenStringIsPalindrome_ShouldReturnTrue(String word) {
         // Arrange
+        boolean isPalindrome = false;
+
         // Act
+        isPalindrome = LongestPalindromicSubstring.isPalindrome(word);
+
         // Assert
+        assertTrue(isPalindrome);
     }
 
-    @Test
-    public void isPalindrome_WhenStringIsNotPalindrome_ShouldReturnFalse() {
+    @ParameterizedTest
+    @ValueSource(strings = {"reflection", "stable", "sphere", "mood", "fuel", "park", "image", "or", "you", "trust", "kindgom"})
+    public void isPalindrome_WhenStringIsNotPalindrome_ShouldReturnFalse(String word) {
         // Arrange
+        boolean isPalindrome = false;
+
         // Act
+        isPalindrome = LongestPalindromicSubstring.isPalindrome(word);
+        
         // Assert
+        assertTrue(!isPalindrome);
     }
 }
