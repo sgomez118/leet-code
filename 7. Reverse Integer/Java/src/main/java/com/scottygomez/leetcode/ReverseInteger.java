7294408a9854364d3c4194005741116c86efcf17
Scotty Gomez <scottygomez007@gmail.com>
2020-12-24 13:25:48 -0800

    Problem 7: Implement solution to reverse integer

--- 7. Reverse Integer/Java/src/main/java/com/scottygomez/leetcode/ReverseInteger.java
+++ 7. Reverse Integer/Java/src/main/java/com/scottygomez/leetcode/ReverseInteger.java
@@ -1,5 +1,26 @@
 package com.scottygomez.leetcode;
 
 public class ReverseInteger {
-    
+    public int reverse(int x) {
+        long reversed = 0;
+
+        // Get the sign of the integer
+        boolean isNegative = x < 0;
+        x = Math.abs(x);
+
+        while (x != 0) {
+            reversed *= 10;
+            reversed += x % 10;
+            x /= 10;
+            // If larger than "32-bits" return 0
+            if (reversed < -2 << 30 || reversed > (2 << 30) - 1) {
+                return 0;
+            }
+        }
+
+        // Put the sign back into place
+        reversed = isNegative ? reversed * -1 : reversed;
+
+        return (int)reversed;
+    }
 }
--- 7. Reverse Integer/Java/src/test/java/com/scottygomez/leetcode/ReverseIntegerTest.java
+++ 7. Reverse Integer/Java/src/test/java/com/scottygomez/leetcode/ReverseIntegerTest.java
@@ -6,4 +6,68 @@ import org.junit.jupiter.api.Test;
 
 public class ReverseIntegerTest {
     
+    @Test
+    public void reverse_WhenIntIs123_ShouldReturn321() {
+        // Arrange
+        int x = 123;
+        int expected = 321;
+        
+        // Act
+        int actual = new ReverseInteger().reverse(x);
+        
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverse_WhenIntIsNegative123_ShouldReturnNegative321() {
+        // Arrange
+        int x = -123;
+        int expected = -321;
+        
+        // Act
+        int actual = new ReverseInteger().reverse(x);
+        
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverse_WhenIntIs120_ShouldReturn21() {
+        // Arrange
+        int x = 120;
+        int expected = 21;
+        
+        // Act
+        int actual = new ReverseInteger().reverse(x);
+        
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverse_WhenIntIs0_ShouldReturn0() {
+        // Arrange
+        int x = 0;
+        int expected = 0;
+        
+        // Act
+        int actual = new ReverseInteger().reverse(x);
+        
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverse_WhenReversedIntIsLargerThan32Bits_ShouldReturn0() {
+        // Arrange
+        int x = 1534236469;
+        int expected = 0;
+
+        // Act
+        int actual = new ReverseInteger().reverse(x);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
