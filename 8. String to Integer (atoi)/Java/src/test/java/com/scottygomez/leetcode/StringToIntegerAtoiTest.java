9191c095f965bb99f50589122d016063afe4a398
Scotty Gomez <scottygomez007@gmail.com>
2020-12-26 10:26:39 -0800

    Problem 8: Add example problem test cases

--- 8. String to Integer (atoi)/Java/src/test/java/com/scottygomez/leetcode/StringToIntegerAtoiTest.java
+++ 8. String to Integer (atoi)/Java/src/test/java/com/scottygomez/leetcode/StringToIntegerAtoiTest.java
@@ -6,4 +6,68 @@ import org.junit.jupiter.api.Test;
 
 public class StringToIntegerAtoiTest {
     
+    @Test
+    public void myAtoi_WhenStringIs42_ShouldReturnInteger42() {
+        // Arrange
+        String str = "42";
+        int expected = 42;
+
+        // Act
+        int actual = new StringToIntegerAtoi().myAtoi(str);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void myAtoi_WhenStringContainsLeadingSpaceNegative42_ShouldReturnIntegerNegative42() {
+        // Arrange
+        String str = "   -42";
+        int expected = -42;
+
+        // Act
+        int actual = new StringToIntegerAtoi().myAtoi(str);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void myAtoi_WhenStringContainsWordsAfterNumber4193_ShouldReturnInteger4193() {
+        // Arrange
+        String str = "4193 with words";
+        int expected = 4193;
+
+        // Act
+        int actual = new StringToIntegerAtoi().myAtoi(str);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void myAtoi_WhenStringContainsWordsBeforeNumber987_ShouldReturnInteger0() {
+        // Arrange
+        String str = "words and 987";
+        int expected = 0;
+
+        // Act
+        int actual = new StringToIntegerAtoi().myAtoi(str);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void myAtoi_WhenStringIsLargerThan32Bits_ShouldReturnIntegerMinimumInteger() {
+        // Arrange
+        String str = "-91283472332";
+        int expected = -2 << 30;
+
+        // Act
+        int actual = new StringToIntegerAtoi().myAtoi(str);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
