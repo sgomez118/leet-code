3bb68fa6095c47425cf9d326c0aade2837fa49fe
Scotty Gomez <scottygomez007@gmail.com>
2020-12-30 11:06:57 -0800

    Problem 10: Add test cases for regular expression matching

--- 10. Regular Expression Matching/Java/src/test/java/com/scottygomez/leetcode/RegularExpressionMatchingTest.java
+++ 10. Regular Expression Matching/Java/src/test/java/com/scottygomez/leetcode/RegularExpressionMatchingTest.java
@@ -3,7 +3,91 @@ package com.scottygomez.leetcode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RegularExpressionMatchingTest {
-    
+    @Test
+    public void isMatch_WhenPatternIsAAndStringIsAA_ShouldReturnFalse() {
+        // Arrange
+        String s = "aa";
+        String p = "a";
+        boolean expected = false;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenPatternIsAStarAndStringIsAA_ShouldReturnTrue() {
+        // Arrange
+        String s = "aa";
+        String p = "a*";
+        boolean expected = true;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenPatternIsPeriodStarAndStringIsAB_ShouldReturnTrue() {
+        // Arrange
+        String s = "ab";
+        String p = ".*";
+        boolean expected = true;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenPatternIsCStarAStarBAndStringAAB_ShouldReturnFalse() {
+        // Arrange
+        String s = "aab";
+        String p = "c*a*b";
+        boolean expected = false;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenPatternIsMISStarISStarPStarPeriodAndStringIsMISSISSIPPI_ShouldReturnFalse() {
+        // Arrange
+        String s = "mississippi";
+        String p = "mi*is*p*i";
+        boolean expected = false;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"mississppi", "mississppa", "mississi", "mississ", "missis", "missia", "misis", "miix"})
+    public void isMatch_WhenPatternIsMISStarISStarPStarPeriodAndStringMatchesPattern_ShouldReturnTrue(String s) {
+        // Arrange
+        String p = "mi*is*p*i";
+        boolean expected = false;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
