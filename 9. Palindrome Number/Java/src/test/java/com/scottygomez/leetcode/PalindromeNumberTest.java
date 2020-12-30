448543d2425f1e5059fa4402b67aff3fc6127741
Scotty Gomez <scottygomez007@gmail.com>
2020-12-29 21:26:07 -0800

    Problem 9: Add test cases for is palindrome method

--- 9. Palindrome Number/Java/src/test/java/com/scottygomez/leetcode/PalindromeNumberTest.java
+++ 9. Palindrome Number/Java/src/test/java/com/scottygomez/leetcode/PalindromeNumberTest.java
@@ -6,4 +6,68 @@ import org.junit.jupiter.api.Test;
 
 public class PalindromeNumberTest {
     
+    @Test
+    public void isPalindrome_WhenIntegerIsPalindrome_ShouldReturnTrue() {
+        // Arrange
+        int x = 121;
+        boolean expected = true;
+
+        // Act
+        boolean actual = new PalindromeNumber().isPalindrome(x);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindrome_WhenIntegerIsNegativePalindrome_ShouldReturnFalse() {
+        // Arrange
+        int x = -121;
+        boolean expected = false;
+
+        // Act
+        boolean actual = new PalindromeNumber().isPalindrome(x);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindrome_WhenIntegerIsNotPalindrome_ShouldReturnFalse() {
+        // Arrange
+        int x = 10;
+        boolean expected = false;
+
+        // Act
+        boolean actual = new PalindromeNumber().isPalindrome(x);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindrome_WhenIntegerIsAnotherNegativePalindrome_ShouldReturnFalse() {
+        // Arrange
+        int x = -101;
+        boolean expected = false;
+
+        // Act
+        boolean actual = new PalindromeNumber().isPalindrome(x);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindrome_WhenIntegerIsVeryLargePalindrome_ShouldReturnTrue() {
+        // Arrange
+        int x = 2147447412;
+        boolean expected = true;
+
+        // Act
+        boolean actual = new PalindromeNumber().isPalindrome(x);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
