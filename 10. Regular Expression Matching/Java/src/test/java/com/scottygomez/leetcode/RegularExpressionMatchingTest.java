af99edc5ade5e375b914103af81cf9597e4902c2
Scotty Gomez <scottygomez007@gmail.com>
2020-12-30 16:10:33 -0800

    Problem 10: Add edge test use cases

--- 10. Regular Expression Matching/Java/src/test/java/com/scottygomez/leetcode/RegularExpressionMatchingTest.java
+++ 10. Regular Expression Matching/Java/src/test/java/com/scottygomez/leetcode/RegularExpressionMatchingTest.java
@@ -50,11 +50,11 @@ public class RegularExpressionMatchingTest {
     }
 
     @Test
-    public void isMatch_WhenPatternIsCStarAStarBAndStringAAB_ShouldReturnFalse() {
+    public void isMatch_WhenPatternIsCStarAStarBAndStringAAB_ShouldReturnTrue() {
         // Arrange
         String s = "aab";
         String p = "c*a*b";
-        boolean expected = false;
+        boolean expected = true;
 
         // Act
         boolean actual = new RegularExpressionMatching().isMatch(s, p);
@@ -82,7 +82,91 @@ public class RegularExpressionMatchingTest {
     public void isMatch_WhenPatternIsMISStarISStarPStarPeriodAndStringMatchesPattern_ShouldReturnTrue(String s) {
         // Arrange
         String p = "mi*is*p*i";
+        boolean expected = true;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenStringIsEmptyAndPatternIsEmpty_ShouldReturnTrue() {
+        // Arrange
+        String s = "";
+        String p = "";
+        boolean expected = true;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenStringIsEmptyAndPatternIsNotEmpty_ShouldReturnFalse() {
+        // Arrange
+        String s = "";
+        String p = "a";
         boolean expected = false;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenStringIsNotEmptyAndPatternIsEmpty_ShouldReturnFalse() {
+        // Arrange
+        String s = "aa";
+        String p = "";
+        boolean expected = false;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenStringIsLength20AndPatternIsLength30_ShouldReturnTrue() {
+        // Arrange
+        String s = "bbcccdddeeeedddcccbb";
+        String p = "a*bb*ccc*ddd*eeee*ddd*ccc*bb*.";
+        boolean expected = true;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenStringMatchesPatternAndPatternHasPeriodStarAndMore_ShouldReturnTrue() {
+        // Arrange
+        String s = "cclurkbccddeeeddccbb";
+        String p = ".*a*bb*ccc*ddd*eeee*ddd*ccc*bb";
+        boolean expected = true;
+
+        // Act
+        boolean actual = new RegularExpressionMatching().isMatch(s, p);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isMatch_WhenStringDoesNotMatchePatternAndPatternHasPeriodStarAndMore_ShouldReturnFalse() {
+        // Arrange
+        String s = "clurkbccddeeeddccbbd";
+        String p = ".*a*bb*ccc*ddd*eeee*ddd*ccc*bb";
+        boolean expected = true;
 
         // Act
         boolean actual = new RegularExpressionMatching().isMatch(s, p);
