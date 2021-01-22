74a429c75e9a410322a8a450cc39dc516f07e48f
Scotty Gomez <scottygomez007@gmail.com>
2021-01-22 01:14:37 -0800

    Problem 10: Implement regular expression matching

--- 10. Regular Expression Matching/Java/src/main/java/com/scottygomez/leetcode/RegularExpressionMatching.java
+++ 10. Regular Expression Matching/Java/src/main/java/com/scottygomez/leetcode/RegularExpressionMatching.java
@@ -18,73 +18,111 @@ public class RegularExpressionMatching {
         // Set the starting point of the string
         int stringIndex = 0;
 
+        // Set the last .* state
+        int dotStarIndex = 0;
+
         // We want to iterate through the pattern since its going to be the
         // driver to search for repeated characters
-        for (int i = 0; i < p.length(); i++) {
-            char nextCharacterToMatch = p.charAt(i);
+        for (int patternIndex = 0; patternIndex < p.length(); patternIndex++) {
+            // Restart state
+            isMatch = false;
+
+            char nextCharacterToMatch = p.charAt(patternIndex);
 
             isMatchAnyCharacter = nextCharacterToMatch == '.';
 
-            if (isNextCharacterStar(i, p)) {
+            if (isNextCharacterStar(patternIndex, p)) {
                 // Move to next stop character.
-                if (i + 2 < p.length()) {
-                    i++;
-                    
-                } else if (isMatchAnyCharacter) {
-                    // if '.*' is at the end of the string pattern, gobble up remaining string.
-                    isMatch = true;
-                    break;
-                }
-
+                patternIndex++;
                 isMatch0OrMoreCharacters = true;
             }
 
-            // Iterate through string s
-            while (stringIndex < s.length()) {
-                // Pop until length
-                if (isMatchAnyCharacter && isMatch0OrMoreCharacters) {
-                    isMatch = true;
-                    int pull = p.length() - i - 1;
-                    stringIndex = s.length() - pull;
+            // The .* state
+            if (isMatchAnyCharacter && isMatch0OrMoreCharacters) {
+                // Save state in the event we must retract our state
+                dotStarIndex = patternIndex;
 
-                    // String pattern is longer than our actual 
-                    if (stringIndex < 0) {
-                        stringIndex = 0;
-                    }
+                // Find the next non-star state on the pattern
+                nextCharacterToMatch = getNextNonStarPatternCharacter(patternIndex, p);
 
-                    break;
+                // We couldn't find a valid state we had to forcefully reach
+                if (nextCharacterToMatch == Character.MIN_VALUE) {
+                    return true;
                 }
 
-                // Pop character
-                char nextCharacter = s.charAt(stringIndex);
-
-                // Pop 0 or more
-                if (isMatch0OrMoreCharacters) {
-                    isMatch = true;
-                    if (nextCharacter == nextCharacterToMatch) {
-                        stringIndex++;
-                        continue;
+                while (stringIndex < s.length()) {
+                    if (nextCharacterToMatch == s.charAt(stringIndex)) {
+                        isMatch = true;
+                        break;
                     } else {
-                        isMatch0OrMoreCharacters = false;
+                        stringIndex++;
+                    }
+                }
+
+                // If we matched, continue processing
+                if (isMatch) {
+                    continue;
+                }
+
+                // We didn't match and we're at the end of the string
+                if (!isMatch && stringIndex >= s.length()) {
+                    break;
+                }
+            }
+
+            // * state
+            if (isMatch0OrMoreCharacters) {
+                isMatch = true;
+
+                while (stringIndex < s.length()) {
+                    if (nextCharacterToMatch == s.charAt(stringIndex)) {
+                        stringIndex++;
+                    } else {
                         break;
                     }
                 }
 
-                if (nextCharacterToMatch == nextCharacter || isMatchAnyCharacter) {
-                    isMatch = true;
-                    isMatchAnyCharacter = false;
-                    stringIndex++;
+                isMatch0OrMoreCharacters = false;
+
+                // If we reached the end of the string, we overshot
+                if (stringIndex >= s.length() && getNextNonStarPatternCharacter(patternIndex, p) != Character.MIN_VALUE) {
+                    stringIndex--;
+                }
+
+                // Continue if we matched
+                if (isMatch) {
+                    continue;
+                }
+            }
+
+            // Pop next character
+            char nextCharacter = s.charAt(stringIndex);
+
+            // Next state
+            if (nextCharacterToMatch == nextCharacter || isMatchAnyCharacter) {
+                isMatch = true;
+                isMatchAnyCharacter = false;
+                stringIndex++;
+            }
+
+            if (!isMatch) {
+                // We didn't match break out of loop early
+                if (stringIndex >= s.length()) {
                     break;
                 }
 
-                isMatch = false;
-                break;
+                // If we're still in a .* state and got a false positive from above, then revert back to state
+                if (dotStarIndex > 0) {
+                    patternIndex = dotStarIndex;
+                } else {
+                    break;
+                }
             }
+        }
 
-            // We didn't match break out of loop early
-            if (!isMatch) {
-                break;
-            }
+        // We didn't finish matching the entire string
+        if (stringIndex < s.length()) {
+            isMatch = false;
         }
 
         return isMatch;
@@ -92,5 +130,16 @@ public class RegularExpressionMatching {
 
     private boolean isNextCharacterStar(int index, String pattern) {
         return index + 1 < pattern.length() && pattern.charAt(index + 1) == '*';
+    }
+
+    private char getNextNonStarPatternCharacter(int index, String pattern) {
+        while (index < pattern.length()) {
+            if (pattern.charAt(index) == '*' || isNextCharacterStar(index, pattern)) {
+                index++;
+            } else {
+                return pattern.charAt(index);
+            }
+        }
+        return Character.MIN_VALUE;
     }
 }
