a032df1f1c005f40c00cd1bed31c1053ff4e5598
Scotty Gomez <scottygomez007@gmail.com>
2021-01-26 21:31:33 -0800

    Problem 11: Add example test cases

--- 11. Container With Most Water/Java/src/test/java/com/scottygomez/leetcode/ContainerWithMostWaterTest.java
+++ 11. Container With Most Water/Java/src/test/java/com/scottygomez/leetcode/ContainerWithMostWaterTest.java
@@ -7,5 +7,55 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ContainerWithMostWaterTest {
-    
+    @Test
+    public void maxArea_FromExample1_ShouldReturn49() {
+        // Arrange
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        int expected = 49;
+
+        // Act
+        int actual = new ContainerWithMostWater().maxArea(height);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxArea_FromExample2_ShouldReturn1() {
+        // Arrange
+        int[] height = { 1, 1 };
+        int expected = 1;
+
+        // Act
+        int actual = new ContainerWithMostWater().maxArea(height);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxArea_FromExample3_ShouldReturn16() {
+        // Arrange
+        int[] height = { 4, 3, 2, 1, 4 };
+        int expected = 16;
+
+        // Act
+        int actual = new ContainerWithMostWater().maxArea(height);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxArea_FromExample4_ShouldReturn2() {
+        // Arrange
+        int[] height = { 1, 2, 1 };
+        int expected = 2;
+
+        // Act
+        int actual = new ContainerWithMostWater().maxArea(height);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
