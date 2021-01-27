d7d32b432538c907d9406ded5a07e72da0c70f6c
Scotty Gomez <scottygomez007@gmail.com>
2021-01-26 21:12:29 -0800

    Problem 11: Stub out folder and structure for Java

---
+++ 11. Container With Most Water/Java/src/main/java/com/scottygomez/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,7 @@
+package com.scottygomez.leetcode;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        return 0;
+    }
+}
---
+++ 11. Container With Most Water/Java/src/test/java/com/scottygomez/leetcode/ContainerWithMostWaterTest.java
@@ -0,0 +1,11 @@
+package com.scottygomez.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ContainerWithMostWaterTest {
+    
+}
