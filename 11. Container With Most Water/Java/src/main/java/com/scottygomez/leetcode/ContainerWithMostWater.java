d53c4fdaea08eb903edfc569b141b06a02f0cd6c
Scotty Gomez <scottygomez007@gmail.com>
2021-01-29 14:46:33 -0800

    Problem 11: Implement container with most water

--- 11. Container With Most Water/Java/src/main/java/com/scottygomez/leetcode/ContainerWithMostWater.java
+++ 11. Container With Most Water/Java/src/main/java/com/scottygomez/leetcode/ContainerWithMostWater.java
@@ -2,6 +2,34 @@ package com.scottygomez.leetcode;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        return 0;
+        // Make sure that there is a minimum of 2 numbers
+        if (height.length < 2) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            // Need to find the minimum of two because slanting water is not allowed
+            int min = Math.min(height[left], height[right]);
+
+            int area = min * (right - left);
+            
+            if (area > maxArea) {
+                maxArea = area;
+            }
+
+            // Anchor based on tallest
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+
+        return maxArea;
     }
 }
