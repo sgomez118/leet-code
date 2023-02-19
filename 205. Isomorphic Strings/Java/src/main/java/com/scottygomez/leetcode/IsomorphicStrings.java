a7b972426380762c9d3cc5748f01fad45e7ad77d
Scotty Gomez <scottygomez007@gmail.com>
2023-02-19 11:10:01 -0800

    206. Reduced memory footprint to just ASCII character

--- 205. Isomorphic Strings/Java/src/main/java/com/scottygomez/leetcode/IsomorphicStrings.java
+++ 205. Isomorphic Strings/Java/src/main/java/com/scottygomez/leetcode/IsomorphicStrings.java
@@ -1,25 +1,22 @@
 package com.scottygomez.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
         if (s == t) {
             return true;
         }
 
-        Map<Character, Character> sMapToT = new HashMap<>();
-        Map<Character, Character> tMapToS = new HashMap<>();
+        char[] sMapToT = new char[257];
+        char[] tMapToS = new char[257];
 
         for (int i = 0; i < s.length(); i++) {
-            if (!sMapToT.containsKey(s.charAt(i))) {
-                sMapToT.put(s.charAt(i), t.charAt(i));
+            if (sMapToT[s.charAt(i)] == '\u0000') {
+                sMapToT[s.charAt(i)] = t.charAt(i);
             }
-            if (!tMapToS.containsKey(t.charAt(i))) {
-                tMapToS.put(t.charAt(i), s.charAt(i));
+            if (tMapToS[t.charAt(i)] == '\u0000') {
+                tMapToS[t.charAt(i)] = s.charAt(i);
             }
-            if (sMapToT.get(s.charAt(i)) != t.charAt(i) || tMapToS.get(t.charAt(i)) != s.charAt(i)) {
+            if (sMapToT[s.charAt(i)] != t.charAt(i) || tMapToS[t.charAt(i)] != s.charAt(i)) {
                 return false;
             }
         }
